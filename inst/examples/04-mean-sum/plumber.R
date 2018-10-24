6c410a670ad5a21824e56bf29d3ef3012438634d
Chandrabhan <chandubhise99@gmail.com>
2018-10-24 19:27:29 +0530

    Update plumber.R

--- inst/examples/04-mean-sum/plumber.R
+++ inst/examples/04-mean-sum/plumber.R
@@ -14,7 +14,15 @@ hdbconnection <- function(){
   return("helllllooooo")
 }
 
-#* @post /sum
-addTwo <- function(a, b){
-  as.numeric(a) + as.numeric(b)
+#* @get /sam
+addTwo <- function(){
+  as.String("cool") 
+}
+
+
+
+#* @get /jyoti
+addTwo <- function(){
+  result<-"I am new TL" 
+  return(result)
 }
