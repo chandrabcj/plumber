36bac572b17e17fa5547710ea527db6034d2bf26
Chandrabhan <chandubhise99@gmail.com>
2018-10-24 17:38:40 +0530

    Update plumber.R

--- inst/examples/04-mean-sum/plumber.R
+++ inst/examples/04-mean-sum/plumber.R
@@ -4,6 +4,15 @@ normalMean <- function(samples=10){
   mean(10,20,30)
 }
 
+#* @get /connect
+hdbconnection <- function(){
+  library("RODBC")
+  ch<-odbcConnect("HDB",uid="SBSS_39559419764611020834956769394813002379451511639335091671700080450",pwd="Ff3EPhK.dISDXPcOaQlI8uEzylH8EfLEwB-K83WmW40KzGVlYQAms-jqQjExWRAHNhL3pqEsoLWLxpDv9zg3MswEP5NMEGbWLyCbMVuNuBqBL0T7wbgTb1Ecej0R2TUQ")
+  print(ch)
+  res<-sqlFetch(ch,"SCM_IP_RL_INPUT")
+  print(res)
+}
+
 #* @post /sum
 addTwo <- function(a, b){
   as.numeric(a) + as.numeric(b)
