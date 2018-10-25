34bd88163eb1d789014079b6458e00ace958c8f5
Chandrabhan <chandubhise99@gmail.com>
2018-10-25 20:19:20 +0530

    Update sampledemo.R

--- sampledemo.R
+++ sampledemo.R
@@ -3,3 +3,19 @@ sam1 <- function(){
   result<-"hiiiii"
   return(result)
 }
+
+#* @get /mean
+normalMean <- function(){
+  data <- c(10,20,30,40,50)
+#  mean(100,200,300)
+  result<-mean(data)
+  return(result)
+}
+
+#* @get /connect
+hdbconnection <- function(){
+  library("RODBC")
+  ch<-odbcConnect("HDB",uid="SBSS_39559419764611020834956769394813002379451511639335091671700080450",pwd="Ff3EPhK.dISDXPcOaQlI8uEzylH8EfLEwB-K83WmW40KzGVlYQAms-jqQjExWRAHNhL3pqEsoLWLxpDv9zg3MswEP5NMEGbWLyCbMVuNuBqBL0T7wbgTb1Ecej0R2TUQ")
+  res<-sqlFetch(ch,"SCM_IP_RL_INPUT")
+  return("helllllooooo")
+}
