15af9dcf38201c9ae830fa1533765fb861912639
Chandrabhan <chandubhise99@gmail.com>
2018-10-25 20:35:07 +0530

    Update sampledemo.R

--- sampledemo.R
+++ sampledemo.R
@@ -17,5 +17,5 @@ hdbconnection <- function(){
   library("RODBC")
   ch<-odbcConnect("HDB",uid="SBSS_39559419764611020834956769394813002379451511639335091671700080450",pwd="Ff3EPhK.dISDXPcOaQlI8uEzylH8EfLEwB-K83WmW40KzGVlYQAms-jqQjExWRAHNhL3pqEsoLWLxpDv9zg3MswEP5NMEGbWLyCbMVuNuBqBL0T7wbgTb1Ecej0R2TUQ")
 #  res<-sqlFetch(ch,"SCM_IP_RL_INPUT")
-  return(rch)
+  return(ch)
 }
