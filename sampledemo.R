2d5430df7e59ddbebb338afcdad3518127987149
chandrabcj <45280766+chandrabcj@users.noreply.github.com>
2018-12-04 15:36:38 +0530

    Update sampledemo.R

--- sampledemo.R
+++ sampledemo.R
@@ -18,11 +18,11 @@ hdbconnection <- function(){
   #install.packages('RODBC')
   library('RODBC')
   dbconnection <- odbcDriverConnect("Driver=HDBODBC;
-                Server=10.253.133.184; Port=30065; Database=mdca61030; 
-                Uid=SBSS_39559419764611020834956769394813002379451511639335091671700080450; Pwd=Ff3EPhK.dISDXPcOaQlI8uEzylH8EfLEwB-K83WmW40KzGVlYQAms-jqQjExWRAHNhL3pqEsoLWLxpDv9zg3MswEP5NMEGbWLyCbMVuNuBqBL0T7wbgTb1Ecej0R2TUQ")
+                Server=10.253.93.93; Port=30041;
+                Uid=SBSS_78480386041761199546580793035342616646823403339298267992019093579; Pwd=In2szhCMjMhhxMYNxCsO-fvt0H.P-UeZx7As4fhJsz0SSc6hggQ-Aha4CxVPtc.Pz2PKBxb_sw7_DnZf6Pn8UCQdZlGen1GUwF1P3-Klok7ENgjLaFJ_4Uriermh9kdt")
   
-  ch<-odbcConnect("HDB",uid="SBSS_39559419764611020834956769394813002379451511639335091671700080450",pwd="Ff3EPhK.dISDXPcOaQlI8uEzylH8EfLEwB-K83WmW40KzGVlYQAms-jqQjExWRAHNhL3pqEsoLWLxpDv9zg3MswEP5NMEGbWLyCbMVuNuBqBL0T7wbgTb1Ecej0R2TUQ")
-  res<-sqlFetch(ch,"mdca61030")
+  ch<-odbcConnect("HDB",uid="SBSS_78480386041761199546580793035342616646823403339298267992019093579",pwd="In2szhCMjMhhxMYNxCsO-fvt0H.P-UeZx7As4fhJsz0SSc6hggQ-Aha4CxVPtc.Pz2PKBxb_sw7_DnZf6Pn8UCQdZlGen1GUwF1P3-Klok7ENgjLaFJ_4Uriermh9kdt")
+  res<-sqlFetch(ch,"ZSCHEMA")
   
   #dbconnection <- odbcDriverConnect("Driver=HDBODBC;
   #              Server=10.253.133.184; Port=30065; Database=mdca61030; 
